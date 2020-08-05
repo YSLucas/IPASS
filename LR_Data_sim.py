7f678f702258511708db338503a9738f1e8dab4f
YSLucas <lucassiekkotter@gmail.com>
2020-08-06 00:56:27 +0200

    Maakt vector van game data (versie1)

--- LR_Data_sim.py
+++ LR_Data_sim.py
@@ -51,13 +51,39 @@ def dict2Dataframe(data):
 
 # csvModify()
 
-with open('testttt.csv', 'r') as file:
-        reader = csv.reader(file)
-        data =  list(reader)
-        dataDict = []
-        for row in data:
-            if row[1] in dataDict:
-                continue
-            else:
-                dataDict.append(row[1])
-        print(len(dataDict))
+# with open('testttt.csv', 'r') as file:
+#         reader = csv.reader(file)
+#         data =  list(reader)
+#         dataDict = []
+#         for row in data:
+#             if row[1] in dataDict:
+#                 continue
+#             else:
+#                 dataDict.append(row[1])
+#         print(len(dataDict))
+
+def getDataFrame():
+    """
+        Maakt een vector die kan worden gebruikt voor logistische regressie
+    """
+    #   Maakt een dataframe van .csv met data
+    df = pd.read_csv('gamesBrServer.csv')
+
+    #   Zet 'result'-kolom om in dummies, en dropt onnodige kolommen
+    dummie = pd.get_dummies(df, columns=['result'], drop_first=True)
+    dummie = dummie.drop(['server', 'mmr', 'timestamp', 'team_1', 'team_2'], axis='columns')
+    print(dummie.sample(10))
+
+    #   Deze code zorgt ervoor dat de LoL-karakters later op de juiste ingedeeld kunnen worden
+    dummieTeam1 = df['team_1'].str.get_dummies(sep=',')
+    dummieTeam2 = df['team_2'].str.get_dummies(sep=',')
+
+    #   Voegt de dummies van result, team_1 en team_2 samen
+    mergedDummies = pd.concat([dummieTeam1, dummieTeam2.reindex(dummieTeam1.index)], axis=1)
+    result = pd.concat([mergedDummies, dummie], axis=1)
+    # print(dummieTeam1.sample(5))
+    # print(dummieTeam2.sample(5))
+    print(result.sample(5))
+
+# pd.set_option('display.max_columns', None)
+getDataFrame()
